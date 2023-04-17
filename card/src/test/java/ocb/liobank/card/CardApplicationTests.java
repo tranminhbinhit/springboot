bb6c9837b9ea055879b9bec71d24b27451af6b61
tranminhbinh <tranminhbinhit@gmail.com>
2023-04-17 16:38:24 +0700

    first commitdd

---
+++ card/src/test/java/ocb/liobank/card/CardApplicationTests.java
@@ -0,0 +1,13 @@
+package ocb.liobank.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
